64a6b1a5b170a4ff8b0ceaffd0e9a285bc4c7baa
ChiragSharma1 <chiragsharma1441@gmail.com>
2022-02-27 17:05:37 +0530

    Customer can now order.

--- shopowner/admin.py
+++ shopowner/admin.py
@@ -1,8 +1,10 @@
+from ast import Or
 from django.contrib import admin
-from .models import Product, Shop, Shopowner, Notification
+from .models import Product, Shop, Shopowner, Notification, Order
 # Register your models here.
 
 admin.site.register(Shopowner)
 admin.site.register(Shop)
 admin.site.register(Product)
 admin.site.register(Notification)
+admin.site.register(Order)
